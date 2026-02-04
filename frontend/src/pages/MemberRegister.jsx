2a43d62df14ea35835629dbc1220246540c57bd9
nripan <nripanudayan@gmail.com>
2026-02-04 16:04:59 +0530

    added acceptinvite page and completed tracking

--- frontend/src/pages/MemberRegister.jsx
+++ frontend/src/pages/MemberRegister.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 const MemberRegister = () => {
+    const dispatch = useDispatch()
+    
+    const [form,setForm]=useState({
+        name:'',
+        password:'',
+    })
+    const handleinput =(e)=>{
+        const {name,value}=e.target
+        setForm((prev)=>({...prev,[name]:value}))
+    }
+    const handlesubmit =(e)=>{
+        e.preventDefault()
+        dispatch()
+    }
   return (
-    <div>
-      <div className='flex justify-center items-center'>
-        <form action="" className='rounded-xl p-6 shadow-[inset_0_2px_4px_0_rgb(0,0,0,0.2),_0_2px_10px_0_rgb(0,0,0,0.4)] gap-7  h-90 w-100 flex flex-col items-center justify-center '>
-        <h1 className='font-[bold] w-full text-2xl hover:underline duration-300'>Member Registration</h1>
+      <div className='flex min-h-[calc(100vh-160px)] justify-center items-center '>
+        <form onSubmit={handlesubmit} action="" className='rounded-xl p-6 shadow-[inset_0_2px_4px_0_rgb(0,0,0,0.2),_0_2px_10px_0_rgb(0,0,0,0.4)] gap-7  h-90 w-100 flex flex-col items-center justify-center '>
+        <h1 className='font-[bold] w-full text-3xl hover:underline duration-300'>Member Registration</h1>
 
             <div className='flex flex-col w-full '>
-              <label >Member Name</label>
-            <input type="text" className='outline-0'   placeholder='John doe' />
+              <label className='font-[bold] text-[22px] ' >Member Name:</label>
+            <input type="text" onChange={handleinput} name='name' value={form.name} className='outline-0 font-[bold] text-[20px]'   placeholder='John doe' />
             </div>
             <div className='flex flex-col w-full '>
-              <label >Member Password</label>
-            <input type="text" className='outline-0'  placeholder='••••••••' />
+              <label className='font-[bold] text-[22px]' >Member Password:</label>
+            <input type="text" onChange={handleinput} name='password' value={form.password} className='outline-0 font-[bold] text-[20px]  '  placeholder='••••••••' />
             </div>
-            <div>
-                <button>register</button>
+            <div className='w-full'>
+                <button className='p-2 bg-gray-500 w-full rounded-full'>register</button>
             </div>
         </form>
       </div>
-    </div>
   )
 }
 
